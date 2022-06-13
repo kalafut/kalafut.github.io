d6be275185c928b3f425dff4fe4046e715e02dfa
Jim Kalafut <jkalafut@hashicorp.com>
2022-06-13 07:43:04 -0700

    Playing around

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.{html,css,md}"],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['cmu_serifroman', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
